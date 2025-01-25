b394e6cef40df1f1da112af3c654a99f8d3760c6
ShinyFedora13 <fedora13shiny02@gmail.com>
2025-01-25 11:55:54 +0530

    Transaction Implemented

---
+++ backend/Transaction/src/test/java/com/chitfund/Transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitfund.Transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
