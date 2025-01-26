70ff4e681273f16e35c62d4b5600491c588021c6
unknown <tonaantony149@gmail.com>
2025-01-26 18:18:08 +0530

    initial commit for frontend

---
+++ frontend/chit-fund-frontend/src/app/features/groups/group-detail/group-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-detail',
+  imports: [],
+  templateUrl: './group-detail.component.html',
+  styleUrl: './group-detail.component.css'
+})
+export class GroupDetailComponent {
+
+}
