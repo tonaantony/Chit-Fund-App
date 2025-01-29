6401192db998f13eddee00d68b151efd8891fa38
unknown <tonaantony149@gmail.com>
2025-01-30 00:57:25 +0530

    implemented group list and details frontend

---
+++ frontend/chit-fund-frontend/src/app/features/groups/group-list/group-list.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { GroupsListComponent } from './group-list.component';
+
+// describe('GroupListComponent', () => {
+//   let component: GroupsListComponent;
+//   let fixture: ComponentFixture<GroupsListComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [GroupsListComponent]
+//     })
+//     .compileComponents();
+    
+//     fixture = TestBed.createComponent(GroupsListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
